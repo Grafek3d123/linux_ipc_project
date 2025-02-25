01aef6c9fea20c823e147d4347ff8af32fdf5600
Evgenii Reponin <ereponin@gmail.com>
2025-02-25 18:37:12 +0300

    added multi-thread to server

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,3 @@
-// server.cpp
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -7,17 +6,40 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <unistd.h>
+#include <thread>   // Для многопоточности
 
 struct msg_buffer {
     long msg_type;
     char msg_text[11];  // 10 символов + 1 для '\0'
     pid_t msg_PID;
-
 };
 
 class Server {
 private:
     int msgid;
+
+    // Функция для обработки одного сообщения в отдельном потоке
+    void process_message(msg_buffer message) {
+        pid_t client_pid = message.msg_PID;
+        std::string msg_text(message.msg_text);
+
+        std::cout << "Message received: " << msg_text;
+        std::cout << " From PID: " << client_pid << std::endl;
+
+        std::reverse(msg_text.begin(), msg_text.end());  // Разворачиваем текст
+        std::cout << "Message reversed: " << msg_text << std::endl << std::endl;
+
+        // Отправляем ответ клиенту
+        msg_buffer response;
+        response.msg_type = 2;  // Тип ответа
+        strncpy(response.msg_text, msg_text.c_str(), 10);
+        response.msg_text[10] = '\0';
+        response.msg_PID = client_pid;
+
+        if (msgsnd(msgid, &response, sizeof(msg_buffer), 0) == -1) {
+            std::cerr << "Failed to send response to client" << std::endl;
+        }
+    }
 
 public:
     Server() {
@@ -37,28 +59,14 @@ public:
     void run() {
         while (true) {
             msg_buffer message;
+            // Получаем сообщение из очереди
             if (msgrcv(msgid, &message, sizeof(msg_buffer), 1, 0) == -1) {
                 perror("Failed to receive message");
                 continue;
             }
 
-            pid_t client_pid = message.msg_PID;
-            std::string msg_text(message.msg_text);
-
-            std::cout << "Message received: " << msg_text;
-            std::cout << " From PID: " << client_pid << std::endl;
-            std::reverse(msg_text.begin(), msg_text.end());
-            std::cout << "Message reversed: " << msg_text << std::endl << std::endl;
-            
-            msg_buffer response;
-            response.msg_type = 2; // answer type
-            strncpy(response.msg_text, msg_text.c_str(), 10);
-            response.msg_text[10] = '\0';
-            response.msg_PID = client_pid;
-
-            if (msgsnd(msgid, &response, sizeof(msg_buffer), 0) == -1) {
-                std::cerr << "Failed to send response to client" << std::endl;
-            }
+            // Создаем новый поток для обработки этого сообщения
+            std::thread(&Server::process_message, this, message).detach();  // detach позволяет не ждать завершения потока
         }
     }
 };
